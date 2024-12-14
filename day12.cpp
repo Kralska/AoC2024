1514930f3f89aefde4ea73b9d60a0ebdca9c5a9c
Jona Scholz <jonaausdo@gmail.com>
2024-12-14 11:14:54 +0100

    Refactored day12 to use array2d

--- day12.cpp
+++ day12.cpp
@@ -1,45 +1,16 @@
 #include <algorithm>
 #include <cstddef>
 #include <deque>
-#include <fstream>
 #include <iostream>
 #include <map>
 #include <ostream>
-#include <ranges>
 #include <set>
-#include <stdexcept>
 #include <string>
-#include <vector>
 
-template <class T> class Array2D {
-public:
-  Array2D(std::size_t length, std::size_t height, T default_value = {})
-      : length_(length), height_(height) {
-    // Initialize empty, '0' doesn't appear
-    vec_ = std::vector<T>(length_ * height_, default_value);
-  }
-  Array2D(std::size_t length, std::size_t height,
-          std::ranges::input_range auto &&range)
-      : Array2D(length, height) {
-    int idx = 0;
-    std::ranges::for_each(range, [&](auto &val) {
-      vec_.at(idx) = val;
-      ++idx;
-    });
-  }
+#include "array2d.h"
 
-  T &at(std::size_t x, std::size_t y) { return vec_.at(y * length_ + x); }
-  const char &at(int x, int y) const { return vec_[y * length_ + x]; }
-  const std::size_t &height() const { return height_; }
-  const std::size_t &length() const { return length_; }
+using Farm = Array2D::Array2D<char>;
 
-private:
-  std::vector<T> vec_;
-  const std::size_t length_;
-  const std::size_t height_;
-};
-
-using Farm = Array2D<char>;
 struct Point {
   std::size_t x, y;
   Point(std::size_t x, std::size_t y) : x(x), y(y) {}
@@ -49,34 +20,6 @@ struct Point {
 std::ostream &operator<<(std::ostream &os, Point p) {
   os << "(" << p.x << ", " << p.y << ")";
   return os;
-}
-
-void print_farm(const Farm &farm) {
-  for (int y = 0; y < farm.height(); ++y) {
-    for (int x = 0; x < farm.length(); ++x) {
-      std::cout << farm.at(x, y);
-    }
-    std::cout << "\n";
-  }
-}
-
-Farm read_input(std::string file_location) {
-  std::ifstream input_file;
-  input_file.open(file_location);
-  if (!input_file.is_open()) {
-    throw std::invalid_argument("Unable to open file at " + file_location);
-  }
-
-  std::string farm_str = "";
-  std::string line;
-  int length = 0, height = 0;
-  while (std::getline(input_file, line)) {
-    ++height;
-    length = line.size();
-    farm_str += line;
-  }
-  Farm farm(length, height, farm_str);
-  return farm;
 }
 
 template <class T>
@@ -146,7 +89,7 @@ int calculate_circumference(const Region &region) {
 
 int get_cost_of_fences(const Farm &farm) {
   int sum = 0;
-  Array2D<int> is_in_region{farm.length(), farm.height(), 0};
+  Array2D::Array2D<int> is_in_region{farm.length(), farm.height(), 0};
   for (std::size_t y = 0; y < farm.height(); ++y) {
     for (std::size_t x = 0; x < farm.length(); ++x) {
       if (is_in_region.at(x, y) == 0) {
@@ -289,7 +232,7 @@ int calculate_sides(const Region& region){
 
 int get_cost_of_fences_with_bulk_discount(const Farm &farm) {
   int sum = 0;
-  Array2D<int> is_in_region{farm.length(), farm.height(), 0};
+  Array2D::Array2D<int> is_in_region{farm.length(), farm.height(), 0};
   for (std::size_t y = 0; y < farm.height(); ++y) {
     for (std::size_t x = 0; x < farm.length(); ++x) {
       if (is_in_region.at(x, y) == 0) {
@@ -315,7 +258,8 @@ int main(int argc, char **argv) {
   }
 
   // First argument is input file location
-  Array2D farm = read_input(argv[1]);
+  Farm farm = Array2D::read_array<char>(argv[1], [](char c){return c;});
+  // Farm farm = read_input(argv[1]);
 
   // Task 1 : Correctly ordered pages
   int cost_of_fences = get_cost_of_fences(farm);
